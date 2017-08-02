f6196d5e8392753de2dd1823eb194ab635b5f1fc
Nelson Tai <chiahsien@gmail.com>
2017-08-02 23:38:49 +0800

    Initialize project

---
+++ ReduxObjC/Codes/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ReduxObjC
+//
+//  Created by Nelson on 2017/8/2.
+//  Copyright © 2017年 Nelson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ReduxObjC/Supporting Files/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ReduxObjC
+//
+//  Created by Nelson on 2017/8/2.
+//  Copyright © 2017年 Nelson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
